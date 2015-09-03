3ccf1d88c5a8e2e0aa920193b72fba37f604e4c6
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-09-03 22:19:59 +0100

    Integrate `webpack-dev-server` and add handy NPM scripts
    If you run `npm start` and make changes in the source code, they will be
    instantly *refreshed* in the browser. This functionality is often referred to as
    "hot module reloading" (HMR).

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require( "path" )
+var webpack = require( "webpack" )
 var HtmlWebpackPlugin = require( "html-webpack-plugin" )
 var ROOT_PATH = path.resolve( __dirname )
 
@@ -9,8 +10,15 @@ module.exports = {
     filename: "bundle.js"
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       title: "Kanban app"
     })
-  ]
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  }
 }
