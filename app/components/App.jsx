6c5859d009559ebf756a3f4943194ffa61c798a0
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-09-04 15:15:23 +0100

    Introduce concept of state in React component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,19 @@
 import React from "react"
 import Notes from "./Notes.jsx"
-import notes from "../notes.js"
+import notesPacket from "../notes.js"
 
 export default class extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      notes: notesPacket
+    }
+  }
+
   render() {
+    let notes = this.state.notes
+
     return (
       <Notes items={notes} />
     )
