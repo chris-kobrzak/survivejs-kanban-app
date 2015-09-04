45516639f6d168d2c1b4901d41f26a83f60430e1
Chris Kobrzak <chris.kobrzak@gmail.com>
2015-09-04 16:24:55 +0100

    Add ability to add notes (no browser refresh persistence as yet)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,4 @@
+import uuid from "node-uuid"
 import React from "react"
 import Notes from "./Notes.jsx"
 import notesPacket from "../notes.js"
@@ -5,17 +6,39 @@ import notesPacket from "../notes.js"
 export default class extends React.Component {
   constructor(props) {
     super(props)
+    this.bindInstanceMethods()
 
     this.state = {
       notes: notesPacket
     }
   }
 
+  bindInstanceMethods() {
+    this.addNote = this.addNote.bind( this )
+  }
+
   render() {
     let notes = this.state.notes
 
     return (
-      <Notes items={notes} />
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <Notes items={notes} />
+      </div>
     )
+  }
+
+  addNote() {
+    let newNote = this.constructor.generateRandomNote()
+    this.setState({
+      notes: this.state.notes.concat( newNote )
+    })
+  }
+
+  static generateRandomNote() {
+    return {
+      id: uuid.v4(),
+      task: "New task"
+    }
   }
 }
