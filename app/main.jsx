5ab3acce6c8dda37dc2a51566c25f442de4e18d5
Chris Kobrzak <chris.kobrzak@gmail.com>
2016-01-01 16:20:46 +0000

    Upgrade React to 0.14
    Also upgrade other NPM packages, except for Babel.

--- app/main.jsx
+++ app/main.jsx
@@ -1,12 +1,13 @@
 import "./main.css"
 import React from "react"
+import ReactDOM from "react-dom"
 import App from "./components/App.jsx"
 
 function main() {
   let divElem = document.createElement( "div" )
   document.body.appendChild( divElem )
 
-  React.render( <App />, divElem )
+  ReactDOM.render( <App />, divElem )
 }
 
 main()
